aa5e4b383738d2d5b88906a87c5c4ab4ed7e3206
sumedh <rsumedh0077@gmail.com>
2021-11-30 18:51:37 +0530

    Added Setup and Contracts

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomiclabs/hardhat-waffle");
+
+const projectId = "2ef5f3e11d7d4ab6803d4aa378bff4bf";
+const privateKey = fs.readFileSync(".secret").toString();
+
+module.exports = {
+  networks: {
+    hardhat: {
+      cjainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      acccounts: [privateKey],
+    },
+    mainnet: {
+      // Need to update
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      acccounts: [privateKey],
+    },
+  },
+  solidity: "0.8.4",
+};
