6be15b0a04911477149677904366f42b1a521e46
sumedh <rsumedh0077@gmail.com>
2021-11-30 19:18:25 +0530

    Added Tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 
+const fs = require('fs');
+
 const projectId = "2ef5f3e11d7d4ab6803d4aa378bff4bf";
 const privateKey = fs.readFileSync(".secret").toString();
 
